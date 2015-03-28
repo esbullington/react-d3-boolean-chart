76bf46545756c36422b7a1f50882eeac2408de62
Eric S. Bullington <eric.s.bullington@gmail.com>
2015-03-28 10:39:03 -0400

    Remove unneeded functions from datagen

--- utils/datagen.js
+++ utils/datagen.js
@@ -10,82 +10,6 @@ exports.generateArrayOfPoints = function(n) {
   return data;
 };
 
-exports.generateArrayOfNameObjects = function(n) {
-  var names = [
-    "Henrietta",
-    "Meaghan",
-    "Miguelina",
-    "Hoyt",
-    "Felecia",
-    "Karisa",
-    "Gaynell",
-    "Meda",
-    "Natisha",
-    "Annika"
-  ];
-  var numbers = [];
-  for (var i = 0; i < n; i++) {
-    var j = Math.floor(Math.random() * 100);
-    numbers.push(j);
-  } 
-  var data = new Array(n);
-  numbers.forEach( function(value, idx) {
-    var i = idx % 10;
-    var name = names[i];
-    var o = {
-      label: name,
-      value: value
-    };
-    data.push(o);
-  });
-  return data;
-};
-
-exports.generatePartsOfWhole = function() {
-  var data = [];
-  var names = [
-    "Henrietta",
-    "Meaghan",
-    "Miguelina",
-    "Hoyt",
-    "Felecia",
-    "Karisa",
-    "Gaynell",
-    "Meda",
-    "Natisha",
-    "Annika"
-  ];
-  var numbers = [];
-  var total = 0;
-  while (total < 100) {
-    var j = Math.floor(Math.random() * 100);
-    var j1 = j/2.0;
-    if (j1 < 10.0) {
-      continue;
-    }
-    total += j1;
-    numbers.push(j1);
-  }
-  numbers.pop();
-  numbers.pop();
-  var sum = numbers.reduce(function(sum, num) {
-    return sum + num;
-  });
-  var remainder = 100 - sum;
-  numbers.push(remainder);
-  var numbers = numbers.sort(function(v) { return v; });
-  numbers.forEach( function(value, idx) {
-    var i = idx % 10;
-    var name = names[i];
-    var o = {
-      label: name,
-      value: value
-    };
-    data.push(o);
-  });
-  return data;
-}
-
 exports.generateArrayOfNumbers = function(n) {
   var data = [];
   for (var i = 0; i < n; i++) {
@@ -115,7 +39,6 @@ exports.generateArrayOfTimeObjects = function(n) {
 
 exports.generateArrayOfDataPointObjects = function(n) {
   // {"id":"MDAwMTg2YTEwMDAxMjRmYTU0ZjU1Y2U0MzcyZmI3NTAwMDAzAA==„,
-  //
   var dataPoints = [75000, 75005, 75010];
 
   function randomDate(start, end) {
@@ -129,13 +52,8 @@ exports.generateArrayOfDataPointObjects = function(n) {
     var timeStamp = randomDate(new Date(), new Date(date.getTime() + 60000));
     var value = Math.random()<.5;
     var datapointId = dataPoints[Math.floor(Math.random() * dataPoints.length)];
-    var point = {"id":"id-" + i, "timeStamp": timeStamp, "stationId":100001,"datapointId": datapointId,"value":value}
-    var timeStamp = randomDate(new Date(), new Date(date.getTime() + 60001));
+    var point = {"id":"id-" + i, "timeStamp": timeStamp, "stationId":100001, "datapointId": datapointId,"value":value}
     data.push(point);
-    if (i > 0) {
-      var point = {"id":"id-" + i, "timeStamp": timeStamp, "stationId":100001,"datapointId": datapointId,"value":data[i-1].value}
-      data.push(point);
-    }
   } 
   return data.sort(function(a, b) {
     return a.timeStamp - b.timeStamp;
